5cb2ed9c76677b155a20a875a74723b9d63d2e5d
Luis Mex <bjt.mex@gmail.com>
2021-06-20 19:09:08 -0500

    deletint

--- django_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/lmex89/Documents/Python/Django/backendtest/django_venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
